7ff0259e1d426b3276915c18a3fa60bca0766d2c
Facundo <facundo.bassi@vcubo.co>
2021-09-23 19:50:37 -0300

    Deleting google analytics

--- VCUBO_G01v05.py
+++ VCUBO_G01v05.py
@@ -449,16 +449,3 @@ if __name__ == "__main__":
     main()
 
 sta.stop_tracking()
-
-google_analytics_js = """
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-179H78TQNX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-179H78TQNX');
-</script>
-    """
-st.components.v1.html(google_analytics_js)
